998bb0de6734ed0c8543dfd659ae7196ae7dddb9
MADNGAME <menschaergerdichnicht@gmx.de>
2015-06-12 13:06:59 +0200

    Describe the functions in the interfaces

--- MenschAergerDichNicht/src/controller/IController.java
+++ MenschAergerDichNicht/src/controller/IController.java
@@ -2,9 +2,42 @@ package controller;
 
 public interface IController {
 
-	public void dice();
+	/*
+	 * This function dice a random number between 1 and 6.
+	 */
 	
-	public boolean move(int idx);
+	void dice();
 	
+	
+	/*
+	 * This function controlls the move option from each
+	 * token. It checks when the player can move
+	 * out of his house or a player can move his token
+	 * on the gamefield.
+	 */
+	
+	boolean move(int idx);
+	
+	/*
+	 * This function check if the token of a player 
+	 * can move out of his startblock.
+	 */
+	boolean getOutOfBlock();
+	
+	/*
+	 * This function return the stonecolor of a player. 
+	 * The stone is on the gamefield. 
+	 */
+	char getTokenColor(int idx);
+	
+	/*
+	 * This function return the color of the player. 
+	 */
+	char getTokenColorBlock(int player, int idx);
+	
+	/*
+	 * This function return the color of block.
+	 */
+	char getTokenColorHouse(int player, int idx);
 	
 }
--- MenschAergerDichNicht/src/model/IPlayer.java
+++ MenschAergerDichNicht/src/model/IPlayer.java
@@ -2,8 +2,23 @@ package model;
 
 public interface IPlayer {
 
+	/*
+	 * This function return the Index of a token. The token
+	 * can be placed on the gamefile, on the playerblock(startposition)
+	 * or on the playerhouse (goal)
+	 */
+	
+	int getIdx();
+	
+	/*
+	 * This function return the name of a player.
+	 */
 	String getName();
 	
+	
+	/*
+	 * This function return the color of a token.
+	 */
 	char getColor();
 	
 	
